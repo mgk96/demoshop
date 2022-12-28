419dc9a508cedb65b820c50207d07ebbda53c009
doccomsa <mkkim1196@naver.com>
2022-12-28 17:51:29 +0900

    네번쨰

--- src/main/java/com/demo/controller/SampleController.java
+++ src/main/java/com/demo/controller/SampleController.java
@@ -1,8 +1,13 @@
 package com.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class SampleController {
-//..
+
+	@GetMapping("/test")
+	public void test() {
+		
+	}
 }
